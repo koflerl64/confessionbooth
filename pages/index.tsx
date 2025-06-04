7f074e528fd734e84848e2e1df08796d4a4d4f4d
koflerl64 <luk.kofler@univie.ac.at>
2025-06-04 08:49:07 +0200

    index.tsx 2nd try

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,22 @@
-// pages/index.tsx
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main className="p-6">
       <h1 className="text-2xl mb-4">Welcome to Confession Booth</h1>
       <ul className="space-y-2">
-        <li><Link href="/upload" className="text-blue-600 underline">Upload Media</Link></li>
-        <li><Link href="/view" className="text-blue-600 underline">View Random Media</Link></li>
+        <li>
+          <Link href="/upload" className="text-blue-600 underline">
+            Upload Media
+          </Link>
+        </li>
+        <li>
+          <Link href="/view" className="text-blue-600 underline">
+            View Random Media
+          </Link>
+        </li>
       </ul>
     </main>
   );
 }
+
